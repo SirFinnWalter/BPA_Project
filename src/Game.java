618dd35e5219e36702a9f9af0f77376b9899e3ed
SirFinn <SirFinnWalter@gmail.com>
2018-09-12 14:13:07 -0400

    public static void main

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,18 @@
 /*
  * =======================================
- * File:             game.java
+ * File:             Game.java
  * Date Created:     09/12/2018 2:09:49 pm
  * Author:           Dakota Taylor
  * -                 -
- * Last Modified:    09/12/2018 2:10:13 pm
+ * Last Modified:    09/12/2018 2:12:24 pm
  * Modified By:      Dakota Taylor
  * =======================================
  */
 
 public class Game {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 
 }
